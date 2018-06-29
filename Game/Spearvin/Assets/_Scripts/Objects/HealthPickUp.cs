ec4b8b272d526fbd665a218b279cccc50002435a
SamTheTotallyNormalPerson <40128566+SamTheTotallyNormalPerson@users.noreply.github.com>
2018-06-29 18:58:54 -0500

    Upadtae 2 6/29/18

---
+++ Game/Spearvin/Assets/_Scripts/Objects/HealthPickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour {
+
+    public GameObject pickupEffect;
+
+    _Spearvin userScript;
+
+    void Start()
+    {
+        userScript = GameObject.FindWithTag("Player").GetComponent<_Spearvin>();
+    }
+
+	void OnTriggerEnter(Collider col)
+
+    {
+       if (col.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply Effect
+        userScript.Health += 1;
+
+        //Remove Power
+        Destroy(gameObject);
+    }
+}
